ebc239acdf176c585361375fcb2a7452194fa492
root <root@localhost>
2025-09-07 18:55:39 +0000

    Implemented BookCollection

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.order(:id)
+  end
+
+  def new
+    @count = Book.count
+    @book = Book.new(id: @count + 1)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save 
+      redirect_to books_path
+    else
+      render('new')
+    end 
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book)
+    else 
+      render('edit')
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+     @book = Book.find(params[:id])
+     @book.destroy
+     redirect_to books_path
+  end
+
+
+  private
+
+  def book_params
+     params.require(:book).permit(
+      :title,
+      :author,
+      :price,
+      :published_date
+    )
+  end 
+end
+
